edc1d878dec1b8e3d7242cb314b5c8b61dc3d128
Igor Andrade <oigorwebs@gmail.com>
2024-04-15 20:37:59 -0300

    Create toast component

---
+++ src/components/Toast.vue
@@ -0,0 +1,28 @@
+<template>
+  <CToaster class="p-3" placement="top-end">
+    <CToast
+      :color="toast.color"
+      class="text-white align-items-center"
+      v-for="(toast, index) in toasts"
+      visible
+      :key="index"
+    >
+      <CToastHeader closeButton>
+        <span class="me-auto fw-bold">{{ toast.title }}</span>
+      </CToastHeader>
+      <CToastBody>
+        {{ toast.content }}
+      </CToastBody>
+    </CToast>
+  </CToaster>
+</template>
+
+<script setup>
+import { ref, defineExpose } from 'vue'
+
+const toasts = ref([])
+
+defineExpose({
+  toasts,
+})
+</script>
