82f387f6a0c34a71166481129c9f2751f8fa3b72
Igor Andrade <oigorwebs@gmail.com>
2024-08-01 16:36:31 -0300

    Add changes

--- src/components/StepProgress.vue
+++ src/components/StepProgress.vue
@@ -1,4 +1,30 @@
 <template>
+  <CModal
+    :visible="isModalVisible"
+    @close="
+      () => {
+        isModalVisible = false
+      }
+    "
+  >
+    <CModalHeader>
+      <CModalTitle>Stop Task</CModalTitle>
+    </CModalHeader>
+    <CModalBody>Are you sure you want to stop this task ? </CModalBody>
+    <CModalFooter>
+      <CButton
+        color="secondary"
+        @click="
+          () => {
+            isModalVisible = false
+          }
+        "
+      >
+        {{ $t('modal.delete.button.cancel') }}
+      </CButton>
+      <CButton color="danger" @click="stopTask">Confirm</CButton>
+    </CModalFooter>
+  </CModal>
   <div>
     <CRow>
       <CCol :sm="12" :lg="12">
@@ -11,27 +37,47 @@
           </CCol>
         </CRow>
         <hr class="mt-0" />
+        <CRow v-if="!error">
+          <div class="d-grid col-6 mx-auto" align="center">
+            <CAlert color="info">
+              <br />
+              <CButton color="danger" variant="outline" class="mb-3" @click="openModal"
+                >STOP TASK</CButton
+              >
+              <br />
 
-        <CRow>
-          <div class="d-grid col-2 mx-auto">
-            <CButton color="danger" variant="outline" class="mb-3">STOP TASK</CButton>
+              <CSpinner color="primary" />
+              <br />Updating task status every 10 seconds...</CAlert
+            >
+          </div>
+        </CRow>
+        <CRow v-else>
+          <div class="d-grid col-6 mx-auto" align="center">
+            <CAlert color="danger">
+              <CIcon icon="cil-sad" size="xxl" /> <br />Sorry, Your task has
+              {{ errorStatus }}!</CAlert
+            >
           </div>
         </CRow>
         <CRow>
           <div>
-            <CProgress>
+            <CProgress class="my-2">
               <CProgressBar :value="progressValue" :max="maxValue"
                 >{{ progressValue }}%</CProgressBar
               >
             </CProgress>
             <div class="steps">
-              <div
-                v-for="(step, index) in steps"
-                :key="index"
-                :class="['step', { active: currentStep === index + 1 }]"
-              >
+              <p v-for="(step, index) in steps" :key="index" align="center">
+                <CAvatar
+                  :color="index + 1 <= currentStep ? 'primary' : 'secondary'"
+                  textColor="white"
+                >
+                  <CIcon icon="cil-check-alt" size="xl" />
+                </CAvatar>
+                <br />
+
                 {{ step }}
-              </div>
+              </p>
             </div>
             <div class="step-content">
               <slot :name="'step-' + currentStep"></slot>
@@ -44,7 +90,11 @@
 </template>
 <script>
 import { CProgress, CProgressBar } from '@coreui/vue'
-import { computed } from 'vue'
+import { onMounted, ref, computed } from 'vue'
+import TaskService from '@/services/task.service'
+
+import { useI18n } from 'vue-i18n'
+import { useToast } from 'vue-toastification'
 
 export default {
   name: 'StepProgress',
@@ -60,20 +110,113 @@ export default {
       type: Array,
       required: true,
     },
-    currentStep: {
-      type: Number,
-      default: 1,
-    },
   },
   setup(props) {
+    const { t } = useI18n({ useScope: 'global' })
+    const toast = useToast()
     const taskId = props.id
     const maxValue = computed(() => props.steps.length)
-    const progressValue = computed(() => (props.currentStep / maxValue.value) * 100)
+    const currentStep = ref(0)
+    const errorStatus = ref('')
+    const error = ref(false)
+    const activeColor = ref('#95a5a6')
+    const loading = ref(true)
+    const mySteps = computed(() => props.steps)
+    const progressValue = computed(() => (currentStep.value / maxValue.value) * 100)
+    const isModalVisible = ref(false)
+
+    const openModal = () => {
+      isModalVisible.value = true
+    }
+
+    const stopTask = () => {
+      TaskService.revoke({ task_id: taskIdToStop.value })
+        .then(() => {
+          toast.success(
+            t('notification.successfulMessage', {
+              entity: t('entity.task'),
+              action: t('notification.actions.revoked'),
+            }),
+          )
+          updateTaskStatus(taskIdToStop.value)
+        })
+        .catch((error) => {
+          console.log(error)
+
+          toast.error(t('notification.errorMessage'))
+        })
+        .finally(() => {
+          isModalVisible.value = false
+        })
+    }
+
+    const fetchTaskStatus = () => {
+      TaskService.getTaskStatus(props.id)
+        .then((response) => {
+          console.log(response)
+          console.log(response.status)
+
+          switch (response.status) {
+            case 1: // Pending
+              activeColor.value = '#95a5a6'
+              currentStep.value = 1
+              break
+            case 2: // Started
+              activeColor.value = '#2980b9'
+              currentStep.value = 2
+              break
+            case 3: // Success
+              loading.value = false
+              activeColor.value = '#27ae60'
+              currentStep.value = 3
+              break
+            case 4: // Failure
+              loading.value = false
+              errorStatus.value = 'Failed'
+              activeColor.value = '#c0392b'
+              currentStep.value = 3
+              currentStep.value = 0
+              error.value = true
+              break
+            case 5: // Revoked
+              loading.value = false
+              errorStatus.value = 'Revoked'
+              activeColor.value = '#c0392b'
+              currentStep.value = 3
+              currentStep.value = 0
+              error.value = true
+              break
+          }
+        })
+        .catch(() => {
+          toast.error(t('notification.errorMessage'))
+        })
+    }
+
+    onMounted(() => {
+      fetchTaskStatus()
+      const interval = setInterval(() => {
+        if (loading.value) {
+          fetchTaskStatus()
+        } else {
+          clearInterval(interval)
+        }
+      }, 10000)
+    })
 
     return {
       taskId,
       maxValue,
       progressValue,
+      loading,
+      mySteps,
+      currentStep,
+      errorStatus,
+      activeColor,
+      error,
+      openModal,
+      isModalVisible,
+      stopTask,
     }
   },
 }
--- src/views/dashboard/tasks/TaskDetail.vue
+++ src/views/dashboard/tasks/TaskDetail.vue
@@ -4,12 +4,7 @@
       <CCard class="mb-4">
         <CCardHeader> <strong>Task Detail</strong> </CCardHeader>
         <CCardBody>
-          <StepProgress :id="projectId" :steps="mySteps" :currentStep="currentStep"></StepProgress>
-          <p>{{ currentStep }} {{ statusName }}</p>
-          <p v-if="loading">
-            <CSpinner as="span" size="sm" aria-hidden="true" /> Updating task status every 10
-            seconds...
-          </p>
+          <StepProgress :id="taskId" :steps="mySteps"></StepProgress>
         </CCardBody>
       </CCard>
     </CCol>
@@ -17,10 +12,7 @@
 </template>
 
 <script>
-import { onMounted, ref, computed } from 'vue'
-import AssemblyService from '@/services/task.service'
-import { useI18n } from 'vue-i18n'
-import { useToast } from 'vue-toastification'
+import { ref } from 'vue'
 import StepProgress from '@/components/StepProgress.vue'
 
 export default {
@@ -34,73 +26,12 @@ export default {
     StepProgress,
   },
   setup(props) {
-    const { t } = useI18n({ useScope: 'global' })
-    const toast = useToast()
-    const loading = ref(true)
-    const projectId = props.id
-    const statusName = ref('Complete task')
-    const activeColor = ref('#95a5a6')
-    const currentStep = ref(0)
-
-    const mySteps = computed(() => ['Schedule Task', 'Start Execution', statusName.value])
-
-    const fetchTaskStatus = () => {
-      AssemblyService.getTaskStatus(props.id)
-        .then((response) => {
-          console.log(response)
-          console.log(response.status)
-
-          switch (response.status) {
-            case 1: // Pending
-              activeColor.value = '#95a5a6'
-              currentStep.value = 1
-              break
-            case 2: // Started
-              activeColor.value = '#2980b9'
-              currentStep.value = 2
-              break
-            case 3: // Success
-              loading.value = false
-              activeColor.value = '#27ae60'
-              currentStep.value = 3
-              break
-            case 4: // Failure
-              loading.value = false
-              statusName.value = 'Failure'
-              activeColor.value = '#c0392b'
-              currentStep.value = 3
-              break
-            case 5: // Revoked
-              loading.value = false
-              statusName.value = 'Revoked'
-              activeColor.value = '#c0392b'
-              currentStep.value = 3
-              break
-          }
-        })
-        .catch((error) => {
-          toast.error(t('notification.errorMessage'))
-        })
-    }
-
-    onMounted(() => {
-      fetchTaskStatus()
-      const interval = setInterval(() => {
-        if (loading.value) {
-          fetchTaskStatus()
-        } else {
-          clearInterval(interval)
-        }
-      }, 10000)
-    })
+    const taskId = props.id
+    const mySteps = ['Schedule Task', 'Start Execution', 'Complete task']
 
     return {
-      loading,
-      projectId,
+      taskId,
       mySteps,
-      currentStep,
-      statusName,
-      activeColor,
     }
   },
 }
